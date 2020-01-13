958abf027429f537d3d7951ebb162503379468ec
Madhur Bhargava <madhur.bhargava333@gmail.com>
2020-01-13 02:12:14 +0100

    Create lc_191_number_of_1_bits.java

---
+++ lc_191_number_of_1_bits.java
@@ -0,0 +1,14 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        while(n != 0) {
+            int result = n & 1;
+            if(result == 1) {
+                count++;
+            }
+            n = n >>> 1;
+        }
+        return count;
+    }
+}
